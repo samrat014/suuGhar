a223786868f8c5721ddd1f6c339df36cc0599f0e
samrat014 <tarmaskarki014@gmail.com>
2022-08-25 12:39:51 +0545

    added sqlquery

--- addtoilets.php
+++ addtoilets.php
@@ -2,21 +2,38 @@
 
 include('config.php');
 
+if(isset($_POST['submit'])){
+    $location =  $_POST['location'];
+    $lat =  $_POST['lat'];
+    $lng =  $_POST['lng'];
+    $description =  $_POST['description'];
+    $picture =  $_POST['picture'];
+    
+echo   $location ; echo $lat ;echo $lng ; echo $description; echo $picture;
+
+        $sql= "INSERT INTO location (location name, lat, lng, description, picture) values ('$location','$lat','$lng','$description','$picture')";
+        
+        if (mysqli_query($conn, $sql)) {
+            echo "New record created successfully";
+          } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+          }
+}
 ?>
 <body>
     <b>Enter the detail of your toilet location </b><br>
 <br>  
-<!-- <form action -->
+<form action="" method="POST">
     <table>
         <tr>
             <th>
            <b>
 
-               location name: <input type="text"><br>
-               latitude:      <input type="number"><br>
-               logitude:      <input type="number"><br>
-               description:   <input type="text"><br>
-               picture:       <input type="file" >
+               location name: <input type="text" name='location'><br>
+               latitude:      <input type="number" name='lat'><br>
+               logitude:      <input type="number" name='lng'><br>
+               description:   <input type="text" name='description'><br>
+               picture:       <input type="file"  name='picture' >
                
             </b>
         </th>
@@ -25,5 +42,5 @@ include('config.php');
 
 <button> <a href="https://www.latlong.net/c/?lat=27.7&long=85.322
 " target="_blank"> get latitude and Longitude</a></button><br>
-<input type="submit">
+<input type="submit" name="submit">
 </body>
